c9f124a1a72c6cfebe770667209015f1ff02d1c5
MMS <35415072+ashenoy25@users.noreply.github.com>
2022-09-08 16:28:57 -0400

    update to App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Manoj
         </a>
       </header>
     </div>
